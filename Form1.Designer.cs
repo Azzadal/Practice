b920a532083939c17528e7c1779a8520b3cd87a6
Azzadal <andr280389@yandex.ru>
2020-07-29 15:38:33 +0700

    wip

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Practice
 {
-    partial class Form1
+    partial class App
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,88 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.createChartButton = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.functionValue = new System.Windows.Forms.TextBox();
             this.cons = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
-            // chart1
+            // chart
             // 
-            this.chart1.BackColor = System.Drawing.Color.DimGray;
-            this.chart1.BackImageTransparentColor = System.Drawing.Color.Black;
-            this.chart1.BackSecondaryColor = System.Drawing.Color.Black;
-            this.chart1.BorderlineColor = System.Drawing.Color.Black;
-            chartArea2.Area3DStyle.IsRightAngleAxes = false;
-            chartArea2.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
-            chartArea2.Area3DStyle.PointDepth = 30;
-            chartArea2.AxisX.InterlacedColor = System.Drawing.Color.White;
-            chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(-1, 0);
-            this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            series3.Legend = "Legend1";
-            series3.LegendText = "Функция";
-            series3.Name = "Series1";
-            series4.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series4.Color = System.Drawing.Color.LimeGreen;
-            series4.Legend = "Legend1";
-            series4.MarkerBorderColor = System.Drawing.Color.Blue;
-            series4.MarkerColor = System.Drawing.Color.Honeydew;
-            series4.MarkerSize = 12;
-            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Star10;
-            series4.Name = "Series2";
-            series4.YValuesPerPoint = 2;
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(1058, 584);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "График функции";
+            this.chart.BackColor = System.Drawing.Color.DimGray;
+            this.chart.BackImageTransparentColor = System.Drawing.Color.Black;
+            this.chart.BackSecondaryColor = System.Drawing.Color.Black;
+            this.chart.BorderlineColor = System.Drawing.Color.Black;
+            chartArea1.Area3DStyle.IsRightAngleAxes = false;
+            chartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            chartArea1.Area3DStyle.PointDepth = 30;
+            chartArea1.AxisX.InterlacedColor = System.Drawing.Color.White;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(-1, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series1.Legend = "Legend1";
+            series1.LegendText = "Функция";
+            series1.Name = "Series1";
+            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.LimeGreen;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Экстремум";
+            series2.MarkerBorderColor = System.Drawing.Color.Blue;
+            series2.MarkerColor = System.Drawing.Color.Honeydew;
+            series2.MarkerSize = 12;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Star10;
+            series2.Name = "Series2";
+            series2.YValuesPerPoint = 2;
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(1058, 584);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "График функции";
             // 
-            // button1
+            // createChartButton
             // 
-            this.button1.Location = new System.Drawing.Point(-1, 590);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 52);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Создать мега-график";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.createChartButton.Location = new System.Drawing.Point(-1, 590);
+            this.createChartButton.Name = "createChartButton";
+            this.createChartButton.Size = new System.Drawing.Size(120, 52);
+            this.createChartButton.TabIndex = 1;
+            this.createChartButton.Text = "Создать график";
+            this.createChartButton.UseVisualStyleBackColor = true;
+            this.createChartButton.Click += new System.EventHandler(this.createChartButton_Click);
             // 
-            // button2
+            // clear
             // 
-            this.button2.Location = new System.Drawing.Point(125, 590);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 52);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Очистить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.clear.Location = new System.Drawing.Point(125, 590);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(95, 52);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
             // 
-            // textBox1
+            // functionValue
             // 
-            this.textBox1.Location = new System.Drawing.Point(236, 590);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 48);
-            this.textBox1.TabIndex = 11;
+            this.functionValue.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.functionValue.Location = new System.Drawing.Point(236, 590);
+            this.functionValue.Multiline = true;
+            this.functionValue.Name = "functionValue";
+            this.functionValue.Size = new System.Drawing.Size(100, 48);
+            this.functionValue.TabIndex = 11;
             // 
             // cons
             // 
+            this.cons.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.cons.Location = new System.Drawing.Point(636, 591);
             this.cons.Multiline = true;
             this.cons.Name = "cons";
@@ -117,19 +121,22 @@
             this.cons.Size = new System.Drawing.Size(421, 125);
             this.cons.TabIndex = 12;
             // 
-            // Form1
+            // App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(1131, 728);
             this.Controls.Add(this.cons);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chart1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.Controls.Add(this.functionValue);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.createChartButton);
+            this.Controls.Add(this.chart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "App";
+            this.Text = "App";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,10 +144,10 @@
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button createChartButton;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox functionValue;
         private System.Windows.Forms.TextBox cons;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,33 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Practice
 {
-    public partial class Form1 : Form
+    public partial class App : Form
     {
-        public Form1()
+        public App()
         {
             InitializeComponent();
         }
 
+        //несколько функций для примера
         public float func(float x)
         {
             //return (float)(4 - Math.Exp(x) - 2 * Math.Pow(x, 2));
-            //return (Math.Sin(Math.PI * x / 180) - 1 / x);
-            //return (float)(Math.Sin(x));
-            return (float)(Math.Pow(x, 2) + 4 * x - 3);
+            //return (float)(Math.Sin(Math.PI * x / 180) - 1 / x);
+            //return (float)Math.Sin(x);
+            //return (float)Math.Cos(x);
+            //return (float)(Math.Pow(x, 2) + 4 * x - 3);
+            //return (float)Math.Pow(x, 3) - 2 * x + 7;
+            return (float)Math.Pow(x, 2);
+            //return (float)x;
             //return (float)Math.Tanh(x);
         }
-        public float FindKoren(float x0, float x1, double eps)
+        public float ExtremumSearch(float x0, float x1, double eps)
         {
             float left = x0, right = x1, x, f;
             int iter = 0;
@@ -41,40 +37,39 @@ namespace Practice
                 else left = x;
                 iter++;
             } while (Math.Abs(f) > eps && iter < 10000);
-            for (int i = -10; i < 30;)
+            for (float i = -10; i < 30;)
             {
-                chart1.Series[0].Points.Add(func(i));
-                i ++;
-                cons.Text += i + Environment.NewLine;
+                chart.Series[0].Points.AddXY(i, func(i));
+                i += 0.1f;
             }
-            chart1.Series[1].Points.Add(func(x));
+            chart.Series[1].Points.AddXY(x, func(x));
 
             cons.Text += iter + " iterations " + func(x);
 
             return func(x);
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void createChartButton_Click(object sender, EventArgs e)
         {
-            chart1.Series[0].Points.Clear();
-            textBox1.Text += FindKoren(0.0f, 6.0f, 0.001).ToString();
-            textBox1.Text += Environment.NewLine;
+            chart.Series[0].Points.Clear();
+            functionValue.Text += ExtremumSearch(0.5f, 5.1f, 0.001).ToString();
+            functionValue.Text += Environment.NewLine;
 
-            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(-20, 20);
-            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
-            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
-            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
-            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            chart.ChartAreas[0].AxisX.ScaleView.Zoom(-20, 20);
+            chart.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
 
-            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(-1, 1);
-            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
-            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
-            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
-            chart1.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+            chart.ChartAreas[0].AxisY.ScaleView.Zoom(-1, 1);
+            chart.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void clear_Click(object sender, EventArgs e)
         {
-            chart1.Series[0].Points.Clear();
+            chart.Series[0].Points.Clear();
         }
     }
 }
